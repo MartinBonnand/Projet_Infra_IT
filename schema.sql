3d9896bd95248f31fb7cecffe16ea52219db7ab5
MartinBonnand <martin1.bonnand@gmail.com>
2026-01-22 10:15:43 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,25 +1,12 @@
+DROP TABLE IF EXISTS taches;
 DROP TABLE IF EXISTS livres;
 DROP TABLE IF EXISTS emprunts;
+DROP TABLE IF EXISTS clients;
 
--- Table des livres
-CREATE TABLE livres (
+CREATE TABLE taches (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     titre TEXT NOT NULL,
-    auteur TEXT NOT NULL,
-    stock INTEGER DEFAULT 1,
-    disponible BOOLEAN DEFAULT 1
+    description TEXT,
+    date_echeance DATE,
+    terminee INTEGER DEFAULT 0
 );
-
--- Table des emprunts pour lier un utilisateur à un livre
-CREATE TABLE emprunts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    livre_id INTEGER,
-    utilisateur TEXT NOT NULL,
-    date_emprunt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (livre_id) REFERENCES livres (id)
-);
-
--- On insère quelques livres pour tester
-INSERT INTO livres (titre, auteur, stock) VALUES ('Le Petit Prince', 'Saint-Exupéry', 3);
-INSERT INTO livres (titre, auteur, stock) VALUES ('1984', 'George Orwell', 1);
-INSERT INTO livres (titre, auteur, stock) VALUES ('Le Seigneur des Anneaux', 'J.R.R. Tolkien', 0);
